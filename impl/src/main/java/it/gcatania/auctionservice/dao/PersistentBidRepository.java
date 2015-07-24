fc22a744dbb660f0904f5906af708b9ad13b6d87
Gabriele Catania <gabriele.ctn@gmail.com>
2015-07-24 12:06:38 +0200

    fix redundant annotation

--- impl/src/main/java/it/gcatania/auctionservice/dao/PersistentBidRepository.java
+++ impl/src/main/java/it/gcatania/auctionservice/dao/PersistentBidRepository.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.RepositoryDefinition;
 
 import it.gcatania.auctionservice.dataobjects.impl.PersistentBid;
 import it.gcatania.auctionservice.dataobjects.impl.PersistentBidKey;
 
-@RepositoryDefinition(domainClass = PersistentBid.class, idClass = PersistentBidKey.class)
 public interface PersistentBidRepository extends CrudRepository<PersistentBid, PersistentBidKey> {
 
   PersistentBid findTopByKeyItem(String itemName, Sort sort);
